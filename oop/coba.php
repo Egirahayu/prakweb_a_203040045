88652efaead12d0cf3ca710358af30ccbdc8a45f
Egirahayu <mohammadegirahayu>
2022-10-02 23:54:56 +0700

    Membuat class dan object

---
+++ oop/coba.php
@@ -0,0 +1,18 @@
+<?php
+// Nama : Mohamad Egi Rahayu
+// NRP : 203040045
+?>
+
+<?php
+
+class Coba
+{
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
